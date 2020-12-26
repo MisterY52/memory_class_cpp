d934469608acefc9e14b94ee941cf13286129b69
MisterY52 <11368116+MisterY52@users.noreply.github.com>
2020-12-26 20:15:37 +0100

    Update

--- memory_class_cpp/example.cpp
+++ memory_class_cpp/example.cpp
@@ -2,13 +2,14 @@
 #include <thread>
 
 Memory m;
-const wchar_t proc_name[] = L"notepad.exe";
 
 static void check_proc_t()
 {
+	const wchar_t proc_name[] = L"notepad.exe";
+
 	while (true)
 	{
-		if (!m.ready())
+		if (m.get_proc_status() != process_status::FOUND_READY)
 		{
 			m.open_proc(proc_name);
 		}
@@ -26,7 +27,8 @@ int main()
 	check_proc.detach();
 
 	printf("Waiting for process...\n");
-	while (!m.ready())
+
+	while (m.get_proc_status() != process_status::FOUND_READY)
 	{
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
@@ -43,16 +45,35 @@ int main()
 	if (m.Write<int>(m.get_proc_baseaddr() + 0x12345, k))
 		printf("Value written\n");
 
-	//Pattern scan
+	//Simple pattern scan
 	BYTE arr[2048];
 	if (m.ReadArray<BYTE>(m.get_proc_baseaddr() + 0x12345, arr, 2048))
 	{
-		DWORD index = m.findPattern(arr, 2048, "74 20 ?? 65 ?? 72");
+		size_t index = findPattern(arr, 2048, "74 20 ?? 65 ?? 72");
 		if (index != -1)
 		{
 			printf("Pattern found at: %p\n", (void*)(m.get_proc_baseaddr() + 0x12345 + index));
 		}
 	}
+
+	//Pattern scan through memory regions with a specific protection
+	uint64_t* results = m.RegionPatternScan(0x20000000000, 0x50000000000, "74 20 ?? 65 ?? 72", 10, PAGE_EXECUTE_READWRITE);
+
+	if (results)
+	{
+		int c = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			if (results[i])
+			{
+				printf("%p\n", (void*)results[i]);
+				c++;
+			}
+		}
+		printf("Found %d results\n", c);
+		delete[] results;
+	}
+	
 
 	//Get the final address from a multi-level pointer
 	uint64_t add = m.ScanPointer(m.get_proc_baseaddr() + 0x12345, new uint32_t[2]{ 0x1d2, 0x345 }, 2);
--- memory_class_cpp/memory.cpp
+++ memory_class_cpp/memory.cpp
@@ -1,25 +1,82 @@
 #include "memory.h"
 
+//Credits: learn_more, stevemk14ebr
+size_t findPattern(const PBYTE rangeStart, size_t len, const char* pattern)
+{
+	size_t l = strlen(pattern);
+	PBYTE patt_base = static_cast<PBYTE>(_malloca(l >> 1));
+	PBYTE msk_base = static_cast<PBYTE>(_malloca(l >> 1));
+	PBYTE pat = patt_base;
+	PBYTE msk = msk_base;
+	if (pat && msk)
+	{
+		l = 0;
+		while (*pattern)
+		{
+			if (*pattern == ' ')
+				pattern++;
+			if (!*pattern)
+				break;
+			if (*(PBYTE)pattern == (BYTE)'\?')
+			{
+				*pat++ = 0;
+				*msk++ = '?';
+				pattern += ((*(PWORD)pattern == (WORD)'\?\?') ? 2 : 1);
+			}
+			else
+			{
+				*pat++ = getByte(pattern);
+				*msk++ = 'x';
+				pattern += 2;
+			}
+			l++;
+		}
+		*msk = 0;
+		pat = patt_base;
+		msk = msk_base;
+		for (size_t n = 0; n < (len - l); ++n)
+		{
+			if (isMatch(rangeStart + n, patt_base, msk_base))
+			{
+				_freea(patt_base);
+				_freea(msk_base);
+				return n;
+			}
+		}
+		_freea(patt_base);
+		_freea(msk_base);
+	}
+	return -1;
+}
+
 uint64_t Memory::get_proc_baseaddr()
 {
 	return proc.baseaddr;
 }
 
-bool Memory::ready()
+process_status Memory::get_proc_status()
 {
-	return found;
+	return status;
 }
 
 void Memory::check_proc()
 {
 	bool c;
-	found = Read<bool>(proc.baseaddr, c);
 
-	if (!found && proc.hProcess)
+	switch (status)
 	{
-		CloseHandle(proc.hProcess);
-		proc.hProcess = NULL;
-		proc.baseaddr = NULL;
+	case process_status::NOT_FOUND:
+		close_proc();
+		break;
+	case process_status::FOUND_READY:
+		if (!Read<bool>(proc.baseaddr, c))
+		{
+			status = process_status::FOUND_NO_ACCESS;
+			close_proc();
+		}
+		break;
+	default:
+		break;
 	}
 }
 
@@ -35,19 +92,45 @@ void Memory::open_proc(const wchar_t* name)
 		{
 			if (wcscmp(entry.szExeFile, name) == 0)
 			{
-				proc.hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
-				proc.baseaddr = GetModuleBaseAddress(entry.th32ProcessID, name);
-				found = true;
+				if (!proc.hProcess)
+				{
+					proc.hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
+
+					if (proc.hProcess)
+					{
+						proc.baseaddr = GetModuleBaseAddress(entry.th32ProcessID, name);
+						status = process_status::FOUND_READY;
+					}
+					else
+					{
+						status = process_status::FOUND_NO_ACCESS;
+					}
+				}
+
+				CloseHandle(snapshot);
+				return;
 			}
 		}
 	}
 
+	status = process_status::NOT_FOUND;
+
 	CloseHandle(snapshot);
 }
 
-uintptr_t Memory::GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+void Memory::close_proc()
 {
-	uintptr_t modBaseAddr = 0;
+	if (proc.hProcess)
+	{
+		CloseHandle(proc.hProcess);
+		proc.hProcess = NULL;
+		proc.baseaddr = NULL;
+	}
+}
+
+uint64_t Memory::GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+	uint64_t modBaseAddr = 0;
 	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
 	if (hSnap != INVALID_HANDLE_VALUE)
 	{
@@ -59,7 +142,7 @@ uintptr_t Memory::GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
 			{
 				if (!_wcsicmp(modEntry.szModule, modName))
 				{
-					modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					modBaseAddr = (uint64_t)modEntry.modBaseAddr;
 					break;
 				}
 			} while (Module32Next(hSnap, &modEntry));
@@ -69,7 +152,7 @@ uintptr_t Memory::GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
 	return modBaseAddr;
 }
 
-uint64_t Memory::ScanPointer(uint64_t ptr_address, uint32_t offsets[], int level)
+uint64_t Memory::ScanPointer(uint64_t ptr_address, const uint32_t offsets[], int level)
 {
 	if (!ptr_address)
 		return NULL;
@@ -78,7 +161,7 @@ uint64_t Memory::ScanPointer(uint64_t ptr_address, uint32_t offsets[], int level
 
 	for (int i = 0; i < level; i++)
 	{
-		if (!Read<uint64_t>(lvl, lvl))
+		if (!Read<uint64_t>(lvl, lvl) || !lvl)
 			return NULL;
 		lvl += offsets[i];
 	}
@@ -128,51 +211,64 @@ bool Memory::InjectDll(const char* DllAbsPath)
 	return true;
 }
 
-//Credits: learn_more, stevemk14ebr
-DWORD Memory::findPattern(const PBYTE rangeStart, DWORD len, const char* pattern)
+uint64_t* Memory::RegionPatternScan(uint64_t start_addr, size_t size, const char* pattern, size_t max_results, DWORD page_prot)
 {
-	size_t l = strlen(pattern);
-	PBYTE patt_base = static_cast<PBYTE>(_malloca(l >> 1));
-	PBYTE msk_base = static_cast<PBYTE>(_malloca(l >> 1));
-	PBYTE pat = patt_base;
-	PBYTE msk = msk_base;
-	if (pat && msk)
+	uint64_t match_addr = 0;
+	BYTE* buffer = NULL;
+	MEMORY_BASIC_INFORMATION mbi = { 0 };
+	uint64_t* result_addr = new uint64_t[max_results]{ 0 };
+	size_t r = 0;
+
+	for (uint64_t addr = start_addr; addr < start_addr + size; addr += mbi.RegionSize)
 	{
-		l = 0;
-		while (*pattern)
+		if (!VirtualQueryEx(proc.hProcess, (LPCVOID)addr, &mbi, sizeof(mbi)))
 		{
-			if (*pattern == ' ')
-				pattern++;
-			if (!*pattern)
-				break;
-			if (*(PBYTE)pattern == (BYTE)'\?')
-			{
-				*pat++ = 0;
-				*msk++ = '?';
-				pattern += ((*(PWORD)pattern == (WORD)'\?\?') ? 2 : 1);
-			}
-			else
-			{
-				*pat++ = getByte(pattern);
-				*msk++ = 'x';
-				pattern += 2;
-			}
-			l++;
+			mbi.RegionSize = 0x1000;
+			continue;
 		}
-		*msk = 0;
-		pat = patt_base;
-		msk = msk_base;
-		for (DWORD n = 0; n < (len - l); ++n)
+
+		if (mbi.State != MEM_COMMIT || mbi.Protect == PAGE_NOACCESS || mbi.Protect == PAGE_GUARD)
+			continue;
+
+		if (page_prot && mbi.Protect != page_prot)
+			continue;
+
+		delete[] buffer;
+		buffer = new BYTE[mbi.RegionSize];
+
+		if (!ReadArray<BYTE>((uint64_t)mbi.BaseAddress, buffer, mbi.RegionSize))
+			continue;
+
+		size_t index = 0;
+		size_t index_sum = 0;
+
+		while (index != -1)
 		{
-			if (isMatch(rangeStart + n, patt_base, msk_base))
+			index = findPattern(buffer + index_sum, mbi.RegionSize - index_sum, pattern);
+
+			if (index != -1)
 			{
-				_freea(patt_base);
-				_freea(msk_base);
-				return n;
+				index_sum += index;
+				match_addr = addr + index_sum;
+				result_addr[r] = match_addr;
+				r++;
+
+				if (r == max_results)
+				{
+					delete[] buffer;
+					return result_addr;
+				}
+
+				index_sum += 1;
 			}
 		}
-		_freea(patt_base);
-		_freea(msk_base);
 	}
-	return -1;
+
+	delete[] buffer;
+
+	if (r > 0)
+		return result_addr;
+
+	delete[] result_addr;
+	return NULL;
 }
